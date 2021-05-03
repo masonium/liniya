897f05702df8c9462b7dc2f96dae28097bfbf885
Mason Smith <masonium@gmail.com>
2021-05-02 20:40:17 -0700

    visitor work, add separate state manip

--- src/scene/scene.rs
+++ src/scene/scene.rs
@@ -1,10 +1,11 @@
 //! A `Scene` is a collection of objects to render.
+use super::visitors::{SceneOcclusionVisitor, CameraVisiblePathCollector};
 use crate::common::*;
 use crate::shape::Shape;
 use crate::{camera::Camera, shape::Path};
 use ncollide3d::bounding_volume::AABB;
 use ncollide3d::{
-    partitioning::{VisitStatus, Visitor, BVH, BVT},
+    partitioning::{BVH, BVT},
     query::Ray,
 };
 
@@ -22,32 +23,84 @@ pub struct SceneBuilder {
 
 impl SceneBuilder {
     pub fn new() -> SceneBuilder {
-	SceneBuilder::default()
+        SceneBuilder::default()
     }
 
     pub fn add<S: Shape + 'static>(mut self, shape: S) -> Self {
-	self.shapes.push(Box::new(shape));
-	self
+        self.shapes.push(Box::new(shape));
+        self
     }
 
     pub fn build(self) -> Scene {
-	Scene::new(self.shapes)
+        Scene::new(self.shapes)
     }
 }
 
-type RenderPath = Vec<Point2<f64>>;
+pub(crate) type RenderPath = Vec<Point2<f64>>;
+
+
+/// Encapsulate the state of the current path being built in
+/// `Scene::render_segment_adaptive`.
+#[derive(Clone, Debug)]
+enum SegmentPathState {
+    /// This line has is not represented on the current path.
+    Empty,
+    /// The last point on the path represents the first
+    /// endpoint of a segment.  The next point should add onto
+    /// this one.
+    Started(Path),
+    /// The last point on the path represents the second
+    /// endpoint of a segment.  Any new endpoints shoudl
+    /// update the last point, rather than changing the
+    /// current point.
+    Continuing(Path)
+}
+
+impl SegmentPathState {
+    /// Update `self` in response to a new point in the path.
+    ///
+    /// When the point provided is `None`, this is interpreted that a
+    /// point that is not visible.
+    fn update(mut self, point: Option<Point3<f64>>) -> (Self, Option<Path>) {
+	use SegmentPathState::*;
+	match (self, point) {
+	    (Empty, Some(p)) => (Started(vec![p]), None),
+	    (Empty, None) => (Empty, None),
+	    (Started(mut path), Some(p)) => {
+		path.push(p);
+		(Continuing(path), None)
+	    },
+	    (Started(path), None) => (Empty, Some(path)),
+	    (Continuing(mut path), Some(p)) => {
+		*path.last_mut().unwrap() = p;
+		(Continuing(path), None)
+	    },
+	    (Continuing(path), None) => (Empty, Some(path))
+	}
+    }
+
+    /// Return the current path if one exists.
+    fn into_path(self) -> Option<Path> {
+	use SegmentPathState::*;
+	match self {
+	    Started(path) | Continuing(path) => Some(path),
+	    Empty => None
+	}
+    }
+}
+
 
 impl Scene {
     pub fn new(shapes: Vec<Box<dyn Shape>>) -> Scene {
-	let shapes_and_bounds = shapes
-	    .into_iter()
-	    .map(|s| {
-		let bb = s.bounding_box();
-		(s, bb)
-	    })
-	    .collect();
-	let bvt = BVT::new_balanced(shapes_and_bounds);
-	Scene { bvt }
+        let shapes_and_bounds = shapes
+            .into_iter()
+            .map(|s| {
+                let bb = s.bounding_box();
+                (s, bb)
+            })
+            .collect();
+        let bvt = BVT::new_balanced(shapes_and_bounds);
+        Scene { bvt }
     }
 
     /// Render a line segment adaptive based on the desired screen resolution.
@@ -59,87 +112,114 @@ impl Scene {
     /// Returns an in-progress path, representing
     /// a (non-proper) prefix of the the current segment.
     fn render_segment_adaptive(
-	self,
-	camera: &Camera,
-	p0: &Point3<f64>,
-	p1: &Point3<f64>,
-	screen_res: f64,
-	paths: &mut Vec<Path>,
-	// The Path currently being built, if one exists.
-	curr_path: &Option<Path>,
+        self,
+        camera: &Camera,
+        p0: &Point3<f64>,
+        p1: &Point3<f64>,
+        screen_res: f64,
+        paths: &mut Vec<Path>,
+        // The Path currently being built, if one exists.
+        curr_path: &Option<Path>,
     ) -> Option<Path> {
-	// Project the segment, split it to the desired resolution,
-	// unproject them, and perform the render.
-	let proj_p0 = camera.project_3d(&p0);
-	let proj_p1 = camera.project_3d(&p1);
+        // Project the segment, split it to the desired resolution,
+        // unproject them, and perform the render.
+        let proj_p0 = camera.project_3d(&p0);
+        let proj_p1 = camera.project_3d(&p1);
 
-	let dist_2d_sq = (proj_p0.xy() - proj_p1.xy()).norm_squared();
+        let dist_2d_sq = (proj_p0.xy() - proj_p1.xy()).norm_squared();
 
-	// poly_proj contains the projected segments
-	let segments: Vec<Point3<f64>> = {
-	    let mut proj_segments = if dist_2d_sq > screen_res * screen_res {
-		let dist_2d = dist_2d_sq.sqrt();
-		let n = (dist_2d / screen_res).ceil() as usize;
-		let v = proj_p1 - proj_p0;
-		(0..=n).map(|i| proj_p0 + v * i as f64 / n as f64).collect()
-	    } else {
-		vec![proj_p0, proj_p1]
-	    };
-	    // unproject the segments so they are back in world space.
-	    for p in proj_segments.iter_mut() {
-		*p = camera.unproject(p);
-	    }
-	    proj_segments
+        // poly_proj contains the projected segments
+        let (segments, proj_segments): (Path, Path) = {
+            let mut proj_segments = if dist_2d_sq > screen_res * screen_res {
+                let dist_2d = dist_2d_sq.sqrt();
+                let n = (dist_2d / screen_res).ceil() as usize;
+                let v = proj_p1 - proj_p0;
+                (0..=n).map(|i| proj_p0 + v * i as f64 / n as f64).collect()
+            } else {
+                vec![proj_p0, proj_p1]
+            };
+	    let segments = proj_segments.iter().map(|p| camera.unproject(p)).collect();
+            (segments, proj_segments)
+        };
+
+
+	// 
+        let curr_path: Path = curr_path.as_ref().cloned().unwrap_or_else(|| vec![]);
+	let (first_point, mut path_state) = if curr_path.is_empty() {
+	    (0, SegmentPathState::Empty)
+	} else {
+	    (1, SegmentPathState::Started(curr_path))
 	};
-	let mut curr_path: Path = curr_path.as_ref().cloned().unwrap_or_else(|| vec![]);
+	
 
-	None
+	for i in first_point..segments.len() {
+	    let is_visible = self.is_point_visible(camera, &segments[i], proj_segments[i]);
+	    
+	    let (new_path_state, finished_path) = path_state.update(if is_visible { Some(segments[i]) } else { None });
+	    if let Some(fp) = finished_path {
+		paths.push(fp);
+	    }
+	    path_state = new_path_state;
+	}
+
+	path_state.into_path()
     }
 
     /// Given a point that we otherwise expect to be able to render
     /// (in the camera frustum, etc.), return true if the point is not
     /// occluded by any other shapes.
-    fn is_point_occluded(
-	&self,
-	camera: &Camera,
-	point: &Point3<f64>,
-	proj_point: impl Into<Option<Point3<f64>>>,
+    fn is_point_visible(
+        &self,
+        camera: &Camera,
+        point: &Point3<f64>,
+        proj_point: impl Into<Option<Point3<f64>>>,
     ) -> bool {
-	// project the point into 2d
-	let proj_point = proj_point
-	    .into()
-	    .unwrap_or_else(|| camera.project_3d(&point));
+        // project the point into 2d
+        let proj_point = proj_point
+            .into()
+            .unwrap_or_else(|| camera.project_3d(&point));
 
-	// find the equivalent point projected behind slightly on the
-	// near plane.
-	let proj_origin = Point3::new(proj_point.x, proj_point.y, 0.0);
-	let origin = camera.unproject(&proj_origin);
-	let proj_far = Point3::new(proj_point.x, proj_point.y, 1.0);
-	let far = camera.unproject(&proj_far);
+        // find the equivalent point projected behind slightly on the
+        // near plane.
+        let proj_origin = Point3::new(proj_point.x, proj_point.y, 0.0);
+        let origin = camera.unproject(&proj_origin);
+        let proj_far = Point3::new(proj_point.x, proj_point.y, 1.0);
+        let far = camera.unproject(&proj_far);
+	let unnorm_dir = point - origin;
+	let target_toi = unnorm_dir.norm();
+
+	let ray = Ray::new(origin, unnorm_dir / target_toi);
+	let max_toi = (far - origin).norm();
+
+	let mut sov = SceneOcclusionVisitor::new(&ray, target_toi, max_toi);
+	let res = self.bvt.best_first_search(&mut sov);
+
+	res.map(|(_, is_visible)| is_visible).unwrap_or(true)
     }
 
     /// Render a 3d-path onto one or more 2d paths in normalized
     /// coordinates.
-    pub(crate) fn render_polyline(
-	&self,
-	path: &Vec<Point3<f64>>,
-	camera: &Camera,
+    pub(crate) fn render_path(
+        &self,
+        path: &Vec<Point3<f64>>,
+        camera: &Camera,
+	screen_res: f64,
     ) -> Vec<Vec<Point2<f64>>> {
-	let clipped_paths = camera.clip_path(path);
+        let clipped_paths = camera.clip_path(path);
 
-	let mut prev_point = path[0];
-	let mut prev_project = camera.project(&prev_point);
+        let mut prev_point = path[0];
+        let mut prev_project = camera.project(&prev_point);
 
-	//let mut curr_path = vec![];
+        //let mut curr_path = vec![];
 
-	for i in 1..path.len() {
-	    let point = path[i];
-	    let project = camera.project(&point);
+        for i in 1..path.len() {
+            let point = path[i];
+            let project = camera.project(&point);
 
-	    prev_point = point;
-	}
+            prev_point = point;
+        }
 
-	vec![]
+        vec![]
     }
 
     /// Return a collection of paths that visible from the provided
@@ -148,8 +228,9 @@ impl Scene {
     /// Each path segment is automatically until the segment, as
     /// projected into screen space, meets the desired resolution.
     pub fn render(&self, camera: &Camera, screen_res: f64) -> Vec<RenderPath> {
-	let mut visitor = OnShapePathVisitor::new(self, camera.clone());
-	self.bvt.visit(&mut visitor);
-	visitor.rendered_paths
+        let mut visitor = CameraVisiblePathCollector::new(self, camera.clone(), screen_res);
+        self.bvt.visit(&mut visitor);
+
+        visitor.rendered_paths
     }
 }
--- src/scene/visitors.rs
+++ src/scene/visitors.rs
@@ -1,16 +1,62 @@
 //! Define visitors for use when rendering scenes.
-use crate::{shape::Shape, common::*};
-use ncollide3d::{query::{RayCast, Ray}, partitioning::{BestFirstVisitor, BestFirstVisitStatus}};
+use crate::{
+    camera::Camera,
+    common::*,
+    shape::Shape,
+};
 use na::Isometry;
+use ncollide3d::{
+    partitioning::{BestFirstVisitStatus, BestFirstVisitor, VisitStatus, Visitor},
+    query::{Ray, RayCast},
+};
+use super::{scene::RenderPath, Scene};
+
+/// Visitor for rendering paths within view of the camera
+pub struct CameraVisiblePathCollector<'a> {
+    scene: &'a Scene,
+
+    /// camera space to render from
+    camera: Camera,
+
+    /// screen resolution
+    screen_res: f64,
+
+    /// Final paths rendered by this visitor
+    pub rendered_paths: Vec<RenderPath>,
+}
+
+impl<'a> CameraVisiblePathCollector<'a> {
+    pub fn new(scene: &'a Scene, camera: Camera, screen_res: f64) -> Self {
+        CameraVisiblePathCollector {
+            camera,
+	    screen_res,
+	    scene,
+            rendered_paths: vec![],
+        }
+    }
+}
+
+impl<'a> Visitor<Box<dyn Shape>, AABB<f64>> for CameraVisiblePathCollector<'a> {
+    fn visit(&mut self, bv: &AABB<f64>, data: Option<&Box<dyn Shape>>) -> VisitStatus {
+        if self.camera.is_aabb_visible(bv) {
+            if let Some(shape) = data {
+		for path in shape.paths() {
+		    self.rendered_paths.extend(self.scene.render_path(&path, &self.camera, self.screen_res));
+		}
+            }
+            VisitStatus::Continue
+        } else {
+            VisitStatus::Stop
+        }
+    }
+}
 
 /// Visitor for determining point occlusion.
 ///
 /// Ultimate yields a true value for the `visible` field if the
 /// earliest intersection is coincident with a shape (probably the
 /// shape the path is coincident with.
-pub struct SceneOcclusionVisitor<'b>
-where
-{
+pub struct SceneOcclusionVisitor<'b> {
     /// Ray to be tested.
     ray: &'b Ray<f64>,
 
@@ -21,70 +67,66 @@ where
     max_toi: f64,
 }
 
-impl<'b> SceneOcclusionVisitor<'b>
-{
+impl<'b> SceneOcclusionVisitor<'b> {
     /// Creates a new `RayIntersectionCostFnVisitor`.
     #[inline]
     pub fn new(
-	ray: &'b Ray<f64>,
-	target_toi: f64,
-	max_toi: f64,
-	//broad_phase: &'a dyn BroadPhase<f64, AABB<f64>, Box<dyn Shape>>,
+        ray: &'b Ray<f64>,
+        target_toi: f64,
+        max_toi: f64,
+        //broad_phase: &'a dyn BroadPhase<f64, AABB<f64>, Box<dyn Shape>>,
     ) -> SceneOcclusionVisitor<'b> {
-	SceneOcclusionVisitor {
-	    ray,
-	    target_toi,
-	    max_toi,
-	}
+        SceneOcclusionVisitor {
+            ray,
+            target_toi,
+            max_toi,
+        }
     }
 }
 
-impl<'b> BestFirstVisitor<f64, Box<dyn Shape>, AABB<f64>>
-    for SceneOcclusionVisitor<'b>
-{
+impl<'b> BestFirstVisitor<f64, Box<dyn Shape>, AABB<f64>> for SceneOcclusionVisitor<'b> {
     type Result = bool;
 
     #[inline]
     fn visit(
-	&mut self,
-	best_cost_so_far: f64,
-	bv: &AABB<f64>,
-	data: Option<&Box<dyn Shape>>,
+        &mut self,
+        best_cost_so_far: f64,
+        bv: &AABB<f64>,
+        data: Option<&Box<dyn Shape>>,
     ) -> BestFirstVisitStatus<f64, Self::Result> {
-	if let Some(rough_toi) =
-	    bv.toi_with_ray(&Isometry::identity(), self.ray, self.max_toi, true)
-	{
-	    let mut res = BestFirstVisitStatus::Continue {
-		cost: rough_toi,
-		result: Some(true),
-	    };
+        if let Some(rough_toi) =
+            bv.toi_with_ray(&Isometry::identity(), self.ray, self.max_toi, true)
+        {
+            let mut res = BestFirstVisitStatus::Continue {
+                cost: rough_toi,
+                result: Some(true),
+            };
 
-	    // If the node has data then it is a leaf
-	    if let Some(shape) = data {
-		// rough_toi is less than or equal the cost of any subnode.
-		// Either: The ray origin is outside the bv, and so no point in the bv
-		//   could have a lower cost than rough_toi.
-		// Or: The ray origin is inside the bv, and rough_toi is 0
-		// We only check the data if it may be better than best_cost_so_far
-		if rough_toi < best_cost_so_far {
-		    if let Some(result) = shape.intersect(self.ray, self.max_toi)
-		    {
-			if result.t < self.target_toi {
-			    res = BestFirstVisitStatus::ExitEarly(Some(false));
-			} else {
-			    res = BestFirstVisitStatus::Continue {
-				cost: result.t,
-				result: Some(true)
-			    };
-			}
-		    }
-		}
-	    }
+            // If the node has data then it is a leaf
+            if let Some(shape) = data {
+                // rough_toi is less than or equal the cost of any subnode.
+                // Either: The ray origin is outside the bv, and so no point in the bv
+                //   could have a lower cost than rough_toi.
+                // Or: The ray origin is inside the bv, and rough_toi is 0
+                // We only check the data if it may be better than best_cost_so_far
+                if rough_toi < best_cost_so_far {
+                    if let Some(result) = shape.intersect(self.ray, self.max_toi) {
+                        if result.t < self.target_toi {
+                            res = BestFirstVisitStatus::ExitEarly(Some(false));
+                        } else {
+                            res = BestFirstVisitStatus::Continue {
+                                cost: result.t,
+                                result: Some(true),
+                            };
+                        }
+                    }
+                }
+            }
 
-	    res
-	} else {
-	    // No intersection so we can ignore all children
-	    BestFirstVisitStatus::Stop
-	}
+            res
+        } else {
+            // No intersection so we can ignore all children
+            BestFirstVisitStatus::Stop
+        }
     }
 }
