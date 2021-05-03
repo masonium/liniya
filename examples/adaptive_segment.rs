f661a71a2b617015001015312c254400e1b33d3f
Mason Smith <masonium@gmail.com>
2021-05-03 00:48:49 -0700

    add basic svg example

---
+++ examples/adaptive_segment.rs
@@ -0,0 +1,56 @@
+extern crate liniya;
+
+use liniya::{
+    camera::Camera,
+    scene::{Scene, SceneBuilder},
+    shape::LatSphere,
+};
+use nalgebra::{Point2, Point3, Vector3};
+
+fn format_polyline(p: &Vec<Point2<f64>>, w: f64, h: f64) -> String {
+    let strs: Vec<String> = p
+        .iter()
+        .map(|p| format!("{:.4},{:.4}", (p.x + 1.0) * w / 2.0, (1.0 - p.y) * h / 2.0))
+        .collect();
+    strs.join(" ")
+}
+
+fn main() {
+    let sphere = LatSphere::new(
+        &Point3::new(0.0, 0.0, 0.0),
+        2.0,
+        std::f64::consts::PI / 10.0,
+    );
+    let sphere2 = LatSphere::new(
+        &Point3::new(-2.0, 2.0, 0.0),
+        2.0,
+        std::f64::consts::PI / 10.0,
+    );
+
+    let scene = SceneBuilder::new().add(sphere).add(sphere2).build();
+
+    let w = 800.0;
+    let h = 600.0;
+    let camera = Camera::new()
+        .look_at(
+            &Point3::new(0.0, 0.0, 5.0),
+            &Point3::new(0.0, 0.0, 0.0),
+            &Vector3::new(0.0, 1.0, 0.0),
+        )
+        .perspective(std::f64::consts::FRAC_PI_2, w / h, 1.0, 10.0)
+        .set_resolution(0.1);
+
+    let r = scene.render(&camera);
+
+    let mut doc =
+        svg::Document::new()
+            .set("width", w)
+            .set("height", h)
+            .add(svg::node::element::Style::new(
+                "polyline { fill: none; stroke-width: 1px; }",
+            ));
+    for path in r {
+        doc = doc.add(svg::node::element::Polyline::new().set("d", format_polyline(&path, w, h)))
+    }
+    println!("{}", doc.to_string())
+}
